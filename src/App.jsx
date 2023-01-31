43fa3507608c7fb20daa5a154698aee694c0fb4a
danijeldzakula <danidzaki060@gmail.com>
2023-01-31 22:22:00 +0100

    update: transition animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import { ImageTransition } from './components/imageTransition/ImageTransition'
 import aries from './assets/aries.svg'
 import cancer from './assets/cancer.svg'
 import capricorn from './assets/capricorn.svg'
 import libra from './assets/libra.svg'
+
+import mercury from './assets/mercury.jpg';
+import pluto from './assets/pluto.jpg';
+import uranus from './assets/uranus.jpg';
+import venus from './assets/venus.jpg';
+
+// const mercury = React.lazy(() => import('./assets/mercury.jpg'));
+// const pluto = React.lazy(() => import('./assets/pluto.jpg'));
+// const uranus = React.lazy(() => import('./assets/uranus.jpg'));
+// const venus = React.lazy(() => import('./assets/venus.jpg'));
+
 import './App.css'
 
 
 
 function App() {
-  const [imageUrl, setImageUrl] = useState('../mercury.jpg');
+  const [imageUrl, setImageUrl] = useState(mercury);
 
   function handleOnChangeImage(image) {
     setImageUrl(image)
@@ -21,21 +32,20 @@ function App() {
         <div className='box'>
           <ImageTransition url={imageUrl} />
 
-          <span onClick={() => handleOnChangeImage('../mercury.jpg')} className='box-item box-item-top'>
+          <span onClick={() => handleOnChangeImage(mercury)} className='box-item box-item-top'>
             <img src={aries} alt="" className='img' />
           </span>
-          <span onClick={() => handleOnChangeImage('../pluto.jpg')} className='box-item box-item-right'>
+          <span onClick={() => handleOnChangeImage(pluto)} className='box-item box-item-right'>
             <img src={cancer} alt="" className='img' />
           </span>
-          <span onClick={() => handleOnChangeImage('../uranus.jpg')} className='box-item box-item-bottom'>
+          <span onClick={() => handleOnChangeImage(uranus)} className='box-item box-item-bottom'>
             <img src={capricorn} alt="" className='img' />
           </span>
-          <span onClick={() => handleOnChangeImage('../venus.jpg')} className='box-item box-item-left'>
+          <span onClick={() => handleOnChangeImage(venus)} className='box-item box-item-left'>
             <img src={libra} alt="" className='img' />
           </span>
         </div>
       </div>
-
     </div>
   )
 }
--- src/components/imageTransition/ImageTransition.jsx
+++ src/components/imageTransition/ImageTransition.jsx
@@ -1,29 +1,29 @@
-import React, { useState } from "react"
+import React, { useState, Suspense } from "react"
 import { Transition } from 'react-transition-group'
 
-const duration = 1000
+const duration = 700
 const defaultStyles = {
-    transition: `opacity ${duration}ms ease-in-out`,
-    opacity: 0
+    transition: `background-image ${duration}ms ease-in-out`,
+    opacity: 1
 }
 const transitionStyles = {
     entering: { opacity: 1 },
     entered: { opacity: 1 },
-    exiting: { opacity: 0 },
-    exited: { opacity: 0 }
+    exiting: { opacity: 1 },
+    exited: { opacity: 1 }
 }
 
 export function ImageTransition({ url }) {
     const [currentUrl, setCurrentUrl] = useState(url)
 
-    console.log(currentUrl)
     console.log(url)
 
     return (
         <Transition in={url === currentUrl} timeout={duration} onExited={() => setCurrentUrl(url)}>
             {(state) => (
-                <figure className="figure-image" style={{ backgroundImage: `url('${currentUrl}')`, ...defaultStyles, ...transitionStyles[state]}}></figure>
+                <figure className="figure-image" style={{ backgroundImage: `url('${currentUrl}')`, ...defaultStyles, ...transitionStyles[state]}} />
             )}
         </Transition>
     )
 }
+
